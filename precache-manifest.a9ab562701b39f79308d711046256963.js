6cb50384cad4a5f9e55c4d6b9b32d1dea9a2dbe6
Shivam Tarone <shivamtarone07@gmail.com>
2020-10-06 13:52:01 +0530

    Updates

---
+++ precache-manifest.a9ab562701b39f79308d711046256963.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "746fb19a724881b1d41a1d2e219fbbb2",
+    "url": "/Food-Recipes-Webapp/index.html"
+  },
+  {
+    "revision": "8d2c4a47ef948479dc97",
+    "url": "/Food-Recipes-Webapp/static/css/main.813e487a.chunk.css"
+  },
+  {
+    "revision": "f2bd51fcd7b838e03ab5",
+    "url": "/Food-Recipes-Webapp/static/js/2.d4728f88.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Food-Recipes-Webapp/static/js/2.d4728f88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d2c4a47ef948479dc97",
+    "url": "/Food-Recipes-Webapp/static/js/main.a8dc66cf.chunk.js"
+  },
+  {
+    "revision": "93dd978a9817e727ff88",
+    "url": "/Food-Recipes-Webapp/static/js/runtime-main.8959b7cd.js"
+  }
+]);
